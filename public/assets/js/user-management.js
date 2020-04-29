959f6e86722edf0866d0bed565465ca4a77a5fab
HiepNg <hiepnguyen2410@gmail.com>
2020-04-29 11:25:50 +0700

    dang lam user management

--- public/assets/js/user-management.js
+++ public/assets/js/user-management.js
@@ -1,83 +1,42 @@
-function updateProfile() {
-    const credentials = localStorage.getItem("credentials");
-    var userInfo = JSON.parse(credentials);
-    const email = userInfo.email;
-    const firstName = document.getElementById("txtFirstName").value;
-    const lastName = document.getElementById("txtLastName").value;
-    const address = document.getElementById("txtAddress").value;
-    const dob = document.getElementById("txtDOB").value;
-    
+function getAllTutor() {
+    var tutorList = [];
 
-    
-}
-
-$(document).ready(function() {
-    const credentials = localStorage.getItem("credentials");
-    var userInfo = JSON.parse(credentials);
-    const isInLoginPage = window.location.pathname === "login.html";
-  
-    var studentLst = null
-    var roleId = 3;
     axios({
         method: "GET",
-        url: host_url + "/user/filter",
+        url: "//34.87.179.204:9090/user/filter",
         headers: {
             'Content-Type': 'application/json'
         },
-        data: {
-            roleId
+        params: {
+            roleId: 2,
+            tutorFlag: false
         }
     })
         .then(function (res) {
-            console.log("1 " +JSON.stringify(res.data.data));
-            console.log("2 " +res.data);
+            tutorList = res.data;
+            createTuTorList(tutorList);
         })
         .catch(function (error) {
-            console.log("3 " + error);
-        });
-return;
-    if (credentials) {
-        if (isInLoginPage) {
-            window.location.assign("index.html");
-        }
-        document.getElementById("txtTutor").value = (userInfo.tutor) ? userInfo.tutor.firstName + " " + userInfo.tutor.lastName : "Not Set" ;
-        document.getElementById("txtFirstName").value = userInfo.firstName;
-        document.getElementById("txtLastName").value = userInfo.lastName;
-        document.getElementById("txtEmail").value = userInfo.email;
-        document.getElementById("txtAddress").value = userInfo.address;
-        document.getElementById("txtDOB").value = formatDate(new Date(Date.parse(userInfo.dob)));
-        document.getElementById("txtFName").innerHTML = userInfo.firstName + " " + userInfo.lastName;
-        return;
-    }    
-});
-
-function changepassword() {
-    const credentials = localStorage.getItem("credentials");
-    var userInfo = JSON.parse(credentials);
-    const email = userInfo.email;
-    const password = document.getElementById("txtoldpassword").value;
-    const newPassword = document.getElementById("txtnewpassword").value;
-
-    axios({
-        method: "POST",
-        url: "http://34.87.179.204:9090/user/updatePassword",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: {
-            email,
-            password,
-            newPassword
-        }
-    })
-        .then(function (res) {
-            alert("Change Password Successfully")
-            $('#changePassword').modal('hide');
-            console.log(res.data)
-        })
-        .catch(function (error) {
-            alert("Wrong Password")
             console.log(error);
-            
-        });
+        })
 }
+
+function createTuTorList(list) {
+    var content = "";
+    for (var i = 0; i < list.length; i++) {
+        const tutor = list[i];
+        content += ` 
+        <tr>
+            <td>${tutor.id}</td>
+            <td>${tutor.firstName}</td>
+            <td>${tutor.lastName}</td>
+            <td>${tutor.gender}</td>
+            <td>` + formatDate(tutor.dob) + `</td>
+            <td>${tutor.email}</td>
+            <td>${tutor.address}</td>
+        </tr>           
+        `;
+    }
+    document.getElementById("tutorListContent").innerHTML = content;
+}
+getAllTutor();
